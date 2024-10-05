f3ed2b5170c8d34613e21aa252ff7f779aede8a4
Jaldy178 <hassan.jaldesa@strathmore.edu>
2024-10-06 01:03:23 +0300

    a23

--- database.php
+++ database.php
@@ -2,6 +2,10 @@
 class database {
     private $host = 'localhost';
     private $db = '';
-    
+    private $user = 'root';
+    private $pass = '';
+    private $charset = 'utf8mb4';
+    private $pdo;
+
 }
 ?>
