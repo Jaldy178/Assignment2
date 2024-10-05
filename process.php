45e9ae8cc986f9339ebb12ca82f7a20ce39d0e89
Jaldy178 <hassan.jaldesa@strathmore.edu>
2024-10-06 00:12:18 +0300

    a7

--- process.php
+++ process.php
@@ -2,3 +2,9 @@ class User {
     private $name;
     private $email;
     private $password;
+
+    public function __construct($name, $email, $password) {
+        $this->name = htmlspecialchars(strip_tags($name));
+        $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $this->password = password_hash($password, PASSWORD_BCRYPT);
+    }
