dcc48f49aac44bf2a5d9c809b57fb0cb1d217e16
Jaldy178 <hassan.jaldesa@strathmore.edu>
2024-10-06 01:07:15 +0300

    a24

--- database.php
+++ database.php
@@ -7,5 +7,13 @@ class database {
     private $charset = 'utf8mb4';
     private $pdo;
 
+    public function connect(){
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        try{
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+            
+        }
+    }
+
 }
 ?>
